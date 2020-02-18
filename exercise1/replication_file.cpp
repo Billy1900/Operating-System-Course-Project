0197d261e64e812e6084dadae3b846987bd07e8e
Billy Luo <32431813+Billy1900@users.noreply.github.com>
2020-02-18 16:32:18 +0800

    Create replication_file.cpp

---
+++ exercise1/replication_file.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <cstring>
+#include <unistd.h>
+
+#define BUFFER_SIZE 1024
+
+int main(int argc, char *argv[]){
+
+    if((argc == 3) && (strcmp(argv[1], argv[2]) != 0)){
+        int fd_src, fd_dest, ret;
+
+        fd_src = open(argv[1], O_RDONLY);
+        if(fd_src < 0){
+            perror("open argv[1]");
+            return -1;
+        }
+
+        fd_dest = open(argv[2], O_WRONLY|O_CREAT, 0755);
+        if(fd_dest < 0){
+            close(fd_src);
+            perror("open argv[2]");
+            return -1;
+        }
+
+        char buf[BUFFER_SIZE] = "";
+        do{
+            memset(buf,0,sizeof(buf));
+            ret = read(fd_src, buf, sizeof(buf));
+            if(ret>0)
+                write(fd_dest, buf, ret);
+        }while(ret >0);
+
+        close(fd_src);
+        close(fd_dest);
+    }
+
+    return 0;
+}
+
