9ca9417e6472cca38dd47939092e7272dfaa9fc0
Billy Luo <32431813+Billy1900@users.noreply.github.com>
2020-05-03 11:24:18 +0800

    Create err.c

---
+++ exercise4/lib/err.c
@@ -0,0 +1,22 @@
+
+#include "err.h"
+
+void err_msg(const char *err) {
+  printf("Error: %s\n", err);
+}
+
+void err_exit(const char *err) {
+  printf("%s error!\n", err);
+  printf("Exit with 1.\n");
+  exit(1);
+}
+
+void usage_err(const char *err) {
+  printf("Usage: %s\n", err);
+  exit(1);
+}
+
+void fatal_err(const char *err) {
+  printf("Fatal: %s\n", err);
+  exit(1);
+}
